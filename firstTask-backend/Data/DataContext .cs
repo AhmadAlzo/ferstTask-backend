513309cc8cc27f3274900a5b2101865d24cd0a01
AhmadAlzo <ahmad1bou7adid@gmail.com>
2023-09-19 11:39:33 +0300

    Add project files.

---
+++ firstTask-backend/Data/DataContext .cs
@@ -0,0 +1,13 @@
+﻿
+namespace firstTask_backend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
